7a3c36144b8eee1b93e28cff0fb7af1165cfeca8
mario <mariocode616@gmail.com>
2026-04-06 15:15:14 -0500

    Vistas agregadas

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 
 public abstract class Persona implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+
 
     private long cedula;
     private String nombre;
